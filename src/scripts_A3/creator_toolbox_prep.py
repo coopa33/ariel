21d8da4d41ef28f2197e9eb8aea156f45ffe2a2c
coopa33 <dan.yu.h97@gmail.com>
2025-09-30 22:43:26 +0000

    Added example usage in __main__

--- src/scripts_A3/creator_toolbox_prep.py
+++ src/scripts_A3/creator_toolbox_prep.py
@@ -1,4 +1,6 @@
 import random
+import numpy as np
+from functools import partial
 from deap import base
 from deap import creator
 from deap import tools
@@ -36,7 +38,7 @@ def register_pop_factories(
     t_attr_name : str,
     t_ind_name : str,
     t_pop_name : str,
-    k : int = 10,
+    k : int = 10
     ) -> None:
     """
     Creates DEAP toolbox function to generate an allele, an individual, and a population. The distribution 
@@ -59,11 +61,31 @@ def register_pop_factories(
         t.register(t_pop_name, tools.initRepeat, list, getattr(t, t_ind_name))
         
 if __name__=="__main__":
+    
+    # Ensure that classes for individual instances are there
     _, ind_type = ensure_deap_types(maximize= False)
-    
     toolbox = base.Toolbox()
-    register_pop_factories(toolbox, ind_type, random.random, "attr_float", "Individual", "Population", 10)
-    pop = toolbox.Population(n = 100)
     
+    # We register two functions, one for generating body populations, and 
+    # one for generating brain populations. Let's say they differ in 
+    # distributions for alleles and number of alleles
+    
+    # Prefil function arguments, to make them zero-argument callables
+    func_gauss = partial(random.gauss, 0.0, 1.0) 
+    func_uniform = partial(random.uniform, -2, 2)
+    
+    # Make the functions (factories) for generating individuals
+    register_pop_factories(
+        toolbox, ind_type, func_gauss, 
+        "attr_float", "IndividualBrain", "PopulationBrain", 10)
+    register_pop_factories(
+        toolbox, ind_type, func_uniform, 
+        "attr_float", "IndividualBody", "PopulationBody", 5)
+    
+    # Generate populations
+    pop_brain = toolbox.PopulationBrain(n = 100)
+    pop_body = toolbox.PopulationBody(n = 100)
+    print(f"An individual from the brain population has length {len(pop_brain[0])}:\n {pop_brain[0]} \n")
+    print(f"An individual from the body population has length {len(pop_body[0])}:\n {pop_body[0]}")
 
     
